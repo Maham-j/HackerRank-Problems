0b961106dfcbec18e63391716a918567fead5464
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-09-16 00:12:00 +0500

    done

---
+++ Count-Apples-and-Oranges/README.md
@@ -0,0 +1,80 @@
+# 🍏🍊 Apple and Orange Counter 🍏🍊
+
+This Python script calculates the number of 🍏 and oranges 🍊 that land on Sam's house based on their positions and distances of falling.
+
+## Table of Contents
+
+- [Problem Statement](#problem-statement)
+- [Usage](#usage)
+- [Input Format](#input-format)
+- [Example](#example)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Problem Statement
+
+Sam's house 🏠 has an apple tree 🌳 and an orange tree 🌳 that yield an abundance of fruit. Given the house's range, the positions of the trees, the distances at which apples and oranges fell, the script determines the number of apples 🍏 and oranges 🍊 that land on Sam's house.
+
+## Usage
+
+1. Make sure you have Python installed on your machine.
+2. Clone or download the repository.
+
+```bash
+git clone https://github.com/your-username/apple-orange-counter.git
+cd apple-orange-counter
+```
+
+3. Run the script:
+
+```bash
+python apple_orange_counter.py
+```
+
+4. Follow the prompts to provide the required input.
+
+## Input Format
+
+The script prompts you to provide the following input:
+
+1. The starting and ending points of the house 🏠 (s, t).
+2. The positions of the apple tree 🌳 and orange tree 🌳 (a, b).
+3. The number of apples 🍏 and oranges 🍊 (m, n).
+4. The distances at which apples fell.
+5. The distances at which oranges fell.
+
+## Example
+
+Suppose you run the script and provide the following input:
+
+```
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+```
+
+The script will output:
+
+```
+1
+1
+```
+
+This means that 1 apple 🍏 and 1 orange 🍊 landed on Sam's house.
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or want to add new features, feel free to open an issue or submit a pull request.
+
+GitHub: [https://github.com/Maham-j]
+
+LinkedIn: [https://www.linkedin.com/in/maham-jamil-268584267]
+
+## License
+
+This project is licensed under the [GNU GENERAL PUBLIC LICENSE](LICENSE).
+
+
+
---
+++ Count-Apples-and-Oranges/count-apples-and-oranges.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# The function to count apples and oranges that land within a house's range
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apples_land = 0  # Counter for apples that land within the range
+    oranges_land = 0  # Counter for oranges that land within the range
+    
+    # Iterate through each distance where apples fell
+    for i in apples:
+        total_apples = i + a  # Calculate the absolute position of the apple
+        if total_apples >= s and total_apples <= t:
+            apples_land += 1  # If apple's position is within the house's range, increment counter
+    
+    print(apples_land)  # Print the count of apples that landed within the house's range
+    
+    # Iterate through each distance where oranges fell
+    for j in oranges:
+        total_oranges = j + b  # Calculate the absolute position of the orange
+        if total_oranges >= s and total_oranges <= t:
+            oranges_land += 1  # If orange's position is within the house's range, increment counter
+            
+    print(oranges_land)  # Print the count of oranges that landed within the house's range
+
+if __name__ == '__main__':
+    # Read the first line of input: house range (s, t)
+    first_multiple_input = input().rstrip().split()
+    s = int(first_multiple_input[0])  # Starting point of the house
+    t = int(first_multiple_input[1])  # Ending point of the house
+    
+    # Read the second line of input: tree locations (a, b)
+    second_multiple_input = input().rstrip().split()
+    a = int(second_multiple_input[0])  # Position of the apple tree
+    b = int(second_multiple_input[1])  # Position of the orange tree
+    
+    # Read the third line of input: number of apples and oranges (m, n)
+    third_multiple_input = input().rstrip().split()
+    m = int(third_multiple_input[0])  # Number of apples
+    n = int(third_multiple_input[1])  # Number of oranges
+    
+    # Read the fourth line of input: distances where apples fell
+    apples = list(map(int, input().rstrip().split()))
+    
+    # Read the fifth line of input: distances where oranges fell
+    oranges = list(map(int, input().rstrip().split()))
