8eb13145c2bb4bdff05e329a1da5062fa26bf875
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-11-03 21:44:23 +0500

    Update main.py

--- Birthday-Candles/main.py
+++ Birthday-Candles/main.py
@@ -3,7 +3,7 @@ def birthdayCakeCandles():
     # Read the number of candles
     n = int(input())
     # Initialize an empty list to store candle heights
-    arr = [] 
+    arr = []  
     # Initialize variables to track tallest candle and count of tallest candles
     tallest_candle = 0
     count = 0
