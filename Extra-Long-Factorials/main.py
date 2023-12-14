d9b6cabf52cd907cbb0c1018926c7ed75e81429b
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-12-14 22:19:45 +0500

    Create main.py

---
+++ Extra-Long-Factorials/main.py
@@ -0,0 +1,22 @@
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    
+    num = 1
+    for i in range(n):
+        fac = num*(n-i)
+        num = fac
+    return num
+        
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    result = extraLongFactorials(n)
+    print(result)
