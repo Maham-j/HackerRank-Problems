3bb12534ea60a15b3a48f36445613b44781fe315
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-02-09 00:28:46 +0500

    Create main.py

---
+++ Word-Order/main.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+
+strings = [input() for _ in range(n)]
+
+dic = {}
+for i in strings:
+    if i not in dic:
+        dic[i] =1
+    else:
+        dic[i] += 1
+
+count = [dic[i] for i in dic]
+print(len(count))
+
+for i in count:
+    print(i,end=' ')
